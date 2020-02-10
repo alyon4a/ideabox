fb6400d9e562e888f88ea0f51a1c2f01345ed8e1
flatironschool <danliu277@gmail.com>
2020-02-10 13:39:34 -0500

    Get ideas from backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
+let url = "http://localhost:3000/"
 let addIdea = false;
 
 window.addEventListener('DOMContentLoaded', () => {
+    getIdeas();
     addNewButtonListener();
 })
 
@@ -12,5 +14,35 @@ function addNewButtonListener() {
         addIdea = !addIdea;
         newIdeaForm.style.display = addIdea ? 'flex' : 'none';
     });
-    
+
+}
+
+function getIdeas() {
+    fetch(`${url}/ideas`)
+        .then(resp => resp.json())
+        .then(ideas => {
+            renderIdeas(ideas)
+        })
+}
+
+function renderIdeas(ideas) {
+    ideas.forEach(idea => {
+        renderIdea(idea)
+    });
+}
+
+function renderIdea(idea) {
+    const ideaCollection = document.getElementById("idea-collection")
+    const div = document.createElement('div')
+    div.classList.add("card")
+    div.innerHTML = `
+    <img class='idea-img' src=${idea.image}>
+    <h3>${idea.title}</h3>
+    <p>${idea.description}</p>
+    <div class='upvote'>
+        <label class='upvote-num'>2</label>
+        <button class='upvote-btn'>^</button>
+    </div>
+    `
+    ideaCollection.appendChild(div)
 }
